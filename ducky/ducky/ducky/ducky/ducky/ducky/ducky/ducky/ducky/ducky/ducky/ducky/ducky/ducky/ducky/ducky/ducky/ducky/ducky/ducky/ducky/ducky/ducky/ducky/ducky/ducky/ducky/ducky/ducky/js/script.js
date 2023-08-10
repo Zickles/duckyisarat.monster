33def2b7567b5ceed8410398a4dc0bcae69cd6e6
Kath <55346310+Kathund@users.noreply.github.com>
2023-08-10 12:21:48 +0800

    Even smaller fix

--- ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/js/script.js
+++ ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/ducky/js/script.js
@@ -5,6 +5,6 @@ window.addEventListener("keyup", (e) => {
   pressed.push(e.key);
   pressed.splice(-code.length - 1, pressed.length - code.length);
   if (pressed.join("").includes(code)) {
-    window.location.replace(window.location.pathname + "ducky");
+    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
   }
 });
